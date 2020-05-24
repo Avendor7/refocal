a98e0890d8a00c952d04207f9d8409c21be972ac
stephen <steve.wiggins7@gmail.com>
2020-05-24 13:46:25 -0400

    new logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import logo from './images/ReFocal.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"></img>
         <input type="text"></input>
       </header>
     </div>
