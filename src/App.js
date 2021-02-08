fe95f2206e72b523cfb1802c4be80c15fc5c894d
stephen <steve.wiggins7@gmail.com>
2021-02-07 20:04:57 -0500

    new converter

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './images/ReFocal.png';
 //import logo from './images/ReFocal2.png';
 import Converter from './converterComponent/converter';
+import NewConverter from './newConverterComponent/newConverter';
 import './App.css';
 
 function App() {
@@ -10,7 +11,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo"></img>
         <br/>
-        <Converter/>
+        <div className="row">
+          <div className="column"><Converter/></div>
+          <div className="column"><NewConverter/></div>
+        </div>
+        
       </header>
     </div>
   );
---
+++ src/newConverterComponent/newConverter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewConverter = () => {
+    return ( 
+        <div className>
+            <h1>Hello World</h1>
+        </div> 
+    );
+}
+ 
+export default NewConverter;
