e51fb15dbcd228e16ecfa41f77f5aabde5a4be97
stephen <steve.wiggins7@gmail.com>
2021-02-16 22:37:02 -0500

    stuff is kinda working?

--- src/newConverterComponent/newConverter.js
+++ src/newConverterComponent/newConverter.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, {useState } from 'react';
+
+const jsondata = require('./lookupTable.json');
 
 const NewConverter = () => {
+
+    let [data, setData] = useState(jsondata);
+
+    const convert = () => {
+        
+        let tempdata = data.map((index)=>(
+            index.values = 4* parseFloat(index.values)
+        ));
+
+        setData(tempdata);
+        console.log(data);
+       
+    };
+    
     return ( 
-        <div className>
-            <h1>Hello World</h1>
+        <div className="converter">
+            
+            {data.map((index)=>(
+                <p>{index.displayName}</p>
+            ))}
+            
+            <button onClick={convert}>button</button>
         </div> 
     );
 }
