4da9a8ad69f165cee8eb7c2284874d5033501a68
Stephen <steve.wiggins7@gmail.com>
2021-02-23 00:23:50 -0500

    functions and notes

--- src/newConverterComponent/newConverter.js
+++ src/newConverterComponent/newConverter.js
@@ -2,25 +2,28 @@ import React, {useState } from 'react';
 
 const jsondata = require('./lookupTable.json');
 
+//use this as the object that gets constantly updated based on the dropdown and data from lookup
+const convertData = {};
+
 const NewConverter = () => {
 
     let [data, setData] = useState(jsondata);
-
-    const convert = () => {
-        
-        let tempdata = data.map((item)=>(
-            item.values = 4* parseFloat(item.values)
-        ));
-
-        setData(tempdata);
-        console.log(data);
-       
-    };
+    //let {convertData, setConvertData} = useState();
   
+    //basically re-run the conversion and display the results with both onChange events
+    const convert = () =>{
+        console.log("hello change");
+
+
+        //TODO grab the data from the input and the dropdown, then grab the conversion array from the state
+        //run conversion setting the state for the converted data. Might have weird problems with the map. map multiple objects/arrays?
+    }
+
+
     return ( 
         <div className="converter">
            <form>
-                <select name="to" id="to" >
+                <select name="to" id="to" onChange={e => convert()}>
                     {data.map((item)=>(
                         <option key={item.name} value={item.name}>{item.displayName}</option>
                     ))}
